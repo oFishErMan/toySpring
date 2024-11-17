01fef12cfc659592760e6d5885452f5677124219
YoungWook <kiki96914@gmail.com>
2024-11-17 22:31:16 +0900

    spring boot 프로젝트 최초 커밋

---
+++ src/main/java/com/vegan/vegan/VeganApplication.java
@@ -0,0 +1,13 @@
+package com.vegan.vegan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vegan/vegan/VeganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegan.vegan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
